aca991753cb1c361c95462f15e7d21696202e3e9
Nathan Nelson <osidenate@gmail.com>
2015-04-17 18:57:28 -0700

    Created Project / Solution

---
+++ SLA-Monitor/LatencyMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatencyMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
