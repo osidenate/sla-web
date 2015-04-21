708da4348c17ea40ad88f5e1cf8df11f7220a606
Nathan Nelson <osidenate@gmail.com>
2015-04-20 20:51:38 -0700

    Created PingProcessor.cs

---
+++ SLA-Monitor/LatencyMonitor/PingProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LatencyMonitor
+{
+    class PingProcessor
+    {
+        public PingProcessor()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
--- SLA-Monitor/LatencyMonitor/Program.cs
+++ SLA-Monitor/LatencyMonitor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LatencyMonitor
@@ -10,6 +11,12 @@ namespace LatencyMonitor
     {
         static void Main(string[] args)
         {
+            var pingProcessor = new PingProcessor();
+
+            pingProcessor.Start();
+
+            Console.WriteLine("Started...");
+            Console.ReadLine();
         }
     }
 }
