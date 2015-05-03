945623d73c9f7b8af762cb223935c8060bb03b81
Nathan Nelson <osidenate@gmail.com>
2015-05-03 10:23:13 -0700

    Renamed namespace

--- SLA-Monitor/LatencyMonitorServices/PingProcessor.cs
+++ SLA-Monitor/LatencyMonitorServices/PingProcessor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LatencyMonitor
+namespace LatencyMonitorServices
 {
     public delegate void PingResponseHandler(PingResponse response);
     public delegate void PingTimeoutHandler(PingResponse response);
---
+++ SLA-Monitor/SLALauncher/Launcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LatencyMonitorServices;
+
+namespace SLALauncher
+{
+    /// <summary>
+    /// Launches the SLA Monitor using the default config found in App.config
+    /// </summary>
+    class Launcher
+    {
+        // Config for the PingProcessor
+        private static string host;
+        private static int pollingInterval;
+        private static int timeout;
+
+        // Config for Firebase
+        private static string firebaseHost;
+
+        static void Main(string[] args)
+        {
+            LoadConfig();
+
+            var latencyMonitor = new LatencyMonitor(host, pollingInterval, timeout);
+            latencyMonitor.Start();
+
+            Console.WriteLine("Started SLA Monitor");
+            Console.WriteLine("Host: " + host);
+
+            Console.ReadLine();
+        }
+
+        private static void LoadConfig()
+        {
+            host = ConfigurationManager.AppSettings["DefaultHost"];
+            pollingInterval = Int32.Parse(ConfigurationManager.AppSettings["DefaultPollingInterval"]);
+            timeout = Int32.Parse(ConfigurationManager.AppSettings["DefaultTimeout"]);
+
+            firebaseHost = "Placeholder Firebase Host";
+        }
+    }
+}
