cc6b6bfe58c471e751c2a2b3dea489c9e216f1e0
Nathan Nelson <nathan.nelson@pearson.com>
2015-05-26 15:08:17 -0700

    Integrated grunt-ts with the project so that typescript files are compiled when we run "grunt"

--- sla-web-client/Gruntfile.js
+++ sla-web-client/Gruntfile.js
@@ -6,27 +6,30 @@ module.exports = function(grunt) {
         tsd: {
             refresh: {
                 options: {
-                    // execute a command
                     command: 'reinstall',
-
-                    //optional: always get from HEAD
                     latest: true,
-
-                    // specify config file
-                    config: 'tsd.json',
-
-                    // experimental: options to pass to tsd.API
-                    opts: {
-                        // props from tsd.Options
-                    }
+                    config: 'tsd.json'
                 }
+            }
+        },
+        ts: {
+            options: {
+                compile: true,                  // perform compilation. [true (default) | false]
+                comments: false,                // same as !removeComments. [true | false (default)]
+                target: 'es5',                  // target javascript language. [es3 (default) | es5]
+                declaration: false              // generate a declaration .d.ts file for every output js file. [true | false (default)]
+            },
+            clientMain: {
+                src: 'src/scripts/app.ts',
+                out: 'src/scripts/app.js'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-tsd');
 
-    grunt.registerTask('build', ['tsd']);
+    grunt.registerTask('setup', ['tsd']);
+    grunt.registerTask('build', ['ts:clientMain']);
     grunt.registerTask('default', ['build']);
-
 };
