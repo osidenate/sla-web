43b8d6e4b4029d0b7cfdf3b13999e0a69ea8e02a
Nathan Nelson <osidenate@gmail.com>
2015-05-25 18:19:55 -0700

    Integrated grunt-tsd with project so that TS type definition files can be imported automatically using grunt

--- sla-web-client/Gruntfile.js
+++ sla-web-client/Gruntfile.js
@@ -3,10 +3,30 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        tsd: {
+            refresh: {
+                options: {
+                    // execute a command
+                    command: 'reinstall',
 
+                    //optional: always get from HEAD
+                    latest: true,
+
+                    // specify config file
+                    config: 'tsd.json',
+
+                    // experimental: options to pass to tsd.API
+                    opts: {
+                        // props from tsd.Options
+                    }
+                }
+            }
+        }
     });
 
-    grunt.registerTask('build', []);
+    grunt.loadNpmTasks('grunt-tsd');
+
+    grunt.registerTask('build', ['tsd']);
     grunt.registerTask('default', ['build']);
 
 };
--- sla-web-client/src/scripts/app.ts
+++ sla-web-client/src/scripts/app.ts
@@ -1,4 +1,5 @@
 
+/// <reference path="../typings/tsd.d.ts" />
 module Main {
     "use strict";
 
