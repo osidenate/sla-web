7f09837db88b499be47bc3fb2889ba0c79c6b8b1
Nathan Nelson <osidenate@gmail.com>
2015-06-07 19:11:03 -0700

    Removed angular2 due to integration issues

--- sla-web-client/src/scripts/app.ts
+++ sla-web-client/src/scripts/app.ts
@@ -1,12 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-import { Component, View, bootstrap } from 'angular2/angular2';
-
-@Component({
-    selector: 'test-app'
-})
-
-
-class MyAppComponent {
-
-}
+(function() {
+    'use strict';
+    angular.module('sla', []);
+})();
