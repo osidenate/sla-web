338c58a5cae9aee05971ac0aed923fa6a8a51031
Nathan Nelson <osidenate@gmail.com>
2015-06-29 14:17:25 -0700

    Status label almost working

--- sla-web-client/src/components/live-monitor/live-monitor.ts
+++ sla-web-client/src/components/live-monitor/live-monitor.ts
@@ -11,8 +11,33 @@ angular.module('sla')
                 },
                 templateUrl: 'components/live-monitor/live-monitor.htm',
                 link: function(scope, iElement, iAttrs) {
-                    var ref = new Firebase(firebaseUrl);
-                    scope.pingInfo = $firebaseObject(ref.child(scope.configId));
+                    // TODO Use $loaded promise to display loading spinner until component is loaded
+
+                    // Setup Firebase bindings
+                    var monitorRef = new Firebase(firebaseUrl + scope.configId);
+                    scope.pingInfo = $firebaseObject(monitorRef);
+
+                    scope.latestPing = $firebaseObject(monitorRef.child('latestPing'));
+
+                    scope.getRtt = function() {
+                        return scope.latestPing.rtt;
+                    };
+
+                    // TODO Shows 'offline' / 'online' fine when page is reloaded but
+                    // does not switch from online to offline when latency monitor stops
+                    // since no firebase event is sent (do we need to using polling instead?) $interval
+                    scope.getStatus = function() {
+                        var latestPingTime = new Date(scope.latestPing.datetime).getTime();
+                        var oneMinuteAgo = Date.now() - (60 * 1000);
+
+                        // Monitor is online if the latest ping is less than a minute old
+                        if (latestPingTime > oneMinuteAgo) {
+                            return 'Live';
+                        }
+                        else {
+                            return 'Offline';
+                        }
+                    };
                 }
             };
         }
