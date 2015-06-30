074cf4cec54af282bb7d5b850efcd9690503fb58
Nathan Nelson <osidenate@gmail.com>
2015-06-29 18:23:09 -0700

    Added loading indicator that is shown while the component is connecting to firebase

--- sla-web-client/src/components/live-monitor/live-monitor.ts
+++ sla-web-client/src/components/live-monitor/live-monitor.ts
@@ -12,26 +12,31 @@ angular.module('sla')
                 },
                 templateUrl: 'components/live-monitor/live-monitor.htm',
                 link: function(scope, iElement, iAttrs) {
-                    // TODO Use $loaded promise to display loading spinner until component is loaded
-
                     var monitorRef = new Firebase(firebaseUrl + scope.configId);
                     var pingInfo = $firebaseObject(monitorRef);
                     var latestPing = $firebaseObject(monitorRef.child('latestPing'));
+                    var updateStatus;
 
-                    var updateStatus = $interval(function() {
-                        // The monitor should be considered offline if it missed three consecutive status updates
-                        // The maximum time between a status update should be (pollInterval + timeout)
-                        // We also add 250ms to take into account network delays
-                        var pollInterval = pingInfo.interval;
-                        var timeout = pingInfo.timeout;
-                        var timedOutLength = 3 * (pollInterval + timeout) + 250;
+                    pingInfo.$loaded()
+                        .then(function() {
+                            scope.finishedLoadingConfig = true;
 
-                        var latestPingTime = new Date(latestPing.datetime).getTime();
-                        var offlineTime = Date.now() - timedOutLength;
+                            updateStatus = $interval(function() {
+                                // The monitor should be considered offline if it missed three consecutive status updates
+                                // The maximum time between a status update should be (pollInterval + timeout)
+                                // We also add 250ms to take into account network delays
+                                var pollInterval = pingInfo.interval;
+                                var timeout = pingInfo.timeout;
+                                var timedOutLength = 3 * (pollInterval + timeout) + 250;
 
-                        scope.monitorStatus = latestPingTime > offlineTime ? 'Online' : 'Offline';
-                    }, 250);
+                                var latestPingTime = new Date(latestPing.datetime).getTime();
+                                var offlineTime = Date.now() - timedOutLength;
 
+                                scope.monitorStatus = latestPingTime > offlineTime ? 'Online' : 'Offline';
+                            }, 250);
+                        });
+
+                    scope.finishedLoadingConfig = false;
                     scope.pingInfo = pingInfo;
                     scope.latestPing = latestPing;
                     scope.monitorStatus = '';
