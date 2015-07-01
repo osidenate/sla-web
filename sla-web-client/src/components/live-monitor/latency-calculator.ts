28aa07fc78cab6960676493936c3c88722299197
Nathan Nelson <osidenate@gmail.com>
2015-06-30 19:13:19 -0700

    Started implementing the Latency Calculator

--- sla-web-client/src/components/live-monitor/latency-calculator.ts
+++ sla-web-client/src/components/live-monitor/latency-calculator.ts
@@ -4,25 +4,42 @@ module LiveMonitor {
      * Stores the latest RTT's in a circular buffer.
      * Calculates the moving average of the pings.
      */
-    class LatencyCalculator {
+
+    export class LatencyCalculator {
         private pointer: number;
         private maxLength: number;
         private buffer: number[];
 
         constructor (length: number) {
+            this.buffer = new Array(length);
             this.maxLength = length;
             this.pointer = 0;
         }
 
-        push (pingRtt: number) {
+        push (pingRtt: number): void {
             this.buffer[this.pointer] = pingRtt;
             this.pointer = (this.pointer + 1) % this.maxLength;
         }
 
-        print () {
-            this.buffer.forEach(function(ele) {
-                console.log(ele);
-            });
-        }
+        //getMovingAverage (): number {
+        //    var buffer = this.buffer;
+        //    var pointer = this.pointer;
+        //    var maxLength = this.maxLength;
+        //
+        //    return buffer.reduce(function(accumulator, current, index) {
+        //        var distanceFromHead: number;
+        //
+        //        if (index < pointer) {
+        //            distanceFromHead = (pointer - 1) - index;
+        //        }
+        //        else {
+        //            distanceFromHead = maxLength + pointer - index;
+        //        }
+        //
+        //        console.info('value: ' + current + ', dist: ' + distanceFromHead);
+        //
+        //        return 0;
+        //    });
+        //}
     }
 }
--- sla-web-client/test/live-monitor/latency-calculator.spec.js
+++ sla-web-client/test/live-monitor/latency-calculator.spec.js
@@ -1,7 +1,26 @@
 'use strict';
 
 describe('Latency Calculator', function() {
-    it('should do some calculations and stuff', function() {
-        expect(true).toBe(true);
+    var latencyCalc;
+
+    afterEach(function() {
+        latencyCalc = null;
+    });
+
+    it('should be able to remember the most recent latencies', function() {
+        latencyCalc = new LiveMonitor.LatencyCalculator(3);
+
+        // The buffer should be full after these latencies are added
+        latencyCalc.push(0);
+        latencyCalc.push(50.2);
+        latencyCalc.push(10000);
+        expect(latencyCalc.buffer).toEqual([0, 50.2, 10000]);
+        expect(latencyCalc.pointer).toEqual(0);
+
+        // The circular buffer should begin to overwrite items when these latencies are added
+        latencyCalc.push(35);
+        latencyCalc.push(20);
+        expect(latencyCalc.buffer).toEqual([35, 20, 10000]);
+        expect(latencyCalc.pointer).toEqual(2);
     });
 });
