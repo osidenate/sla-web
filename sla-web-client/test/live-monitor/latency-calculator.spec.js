da2bbb3e88f91eadc4a399f40a2dedeecaea43df
Nathan Nelson <osidenate@gmail.com>
2015-06-30 19:49:06 -0700

    Created some unit tests to cover the complex operation (moving average calculation) that will be implemented in Latency Calculator.

--- sla-web-client/test/live-monitor/latency-calculator.spec.js
+++ sla-web-client/test/live-monitor/latency-calculator.spec.js
@@ -1,14 +1,11 @@
 'use strict';
 
 describe('Latency Calculator', function() {
+    var LatencyCalculator = LiveMonitor.LatencyCalculator;
     var latencyCalc;
 
-    afterEach(function() {
-        latencyCalc = null;
-    });
-
     it('should be able to remember the most recent latencies', function() {
-        latencyCalc = new LiveMonitor.LatencyCalculator(3);
+        latencyCalc = new LatencyCalculator(3);
 
         // The buffer should be full after these latencies are added
         latencyCalc.push(0);
@@ -22,5 +19,25 @@ describe('Latency Calculator', function() {
         latencyCalc.push(20);
         expect(latencyCalc.buffer).toEqual([35, 20, 10000]);
         expect(latencyCalc.pointer).toEqual(2);
+    });
+
+    it('should be able to calculate the exponential moving average of the most recent latencies', function() {
+        latencyCalc = new LatencyCalculator(3);
+
+        // 100*(1/1) == 100
+        latencyCalc.push(100);
+        expect(latencyCalc.getMovingAverage()).toEqual(100);
+
+        // 100*(1/3) + 50*(2/3) == 66.6666
+        latencyCalc.push(50);
+        expect(latencyCalc.getMovingAverage()).toEqual(66 + (2/3));
+
+        // 100*(1/6) + 50*(2/6) + 150*(3/6) == 108.3333
+        latencyCalc.push(150);
+        expect(latencyCalc.getMovingAverage()).toEqual(108 + (2/3));
+
+        // 50*(1/6) + 150*(2/6) + 25*(3/6) == 70.8333
+        latencyCalc.push(25);
+        expect(latencyCalc.getMovingAverage()).toEqual(70 + (5/6));
     });
 });
