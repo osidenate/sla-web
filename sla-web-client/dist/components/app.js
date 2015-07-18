8298c28151521ddfa3991770ded775966edd4021
Nathan Nelson <osidenate@gmail.com>
2015-07-18 15:41:59 -0700

    Repackaged distribution

--- sla-web-client/dist/components/app.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-var SLAClient;
-(function (SLAClient) {
-    angular.module('sla', ['firebase'])
-        .constant('firebaseUrl', 'https://sla-monitor-dev.firebaseio.com/');
-})(SLAClient || (SLAClient = {}));
